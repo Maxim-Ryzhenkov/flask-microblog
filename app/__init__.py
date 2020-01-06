8ed9ec372df037233b705899b0cdb6e79afcfc33
Maksim Ryzhenkov <ryzhenkov.m@gmail.com>
2020-01-06 05:17:52 +0300

    Добавлен shell_context_processor в __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,10 @@ migrate = Migrate(app, db)
 
 
 from app import routes, models
+from app.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
